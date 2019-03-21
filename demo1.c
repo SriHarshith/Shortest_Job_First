34371f21c1c0003af3193c8d0e250353d1a2adc5
SriHarshith <34154723+SriHarshith@users.noreply.github.com>
2019-03-21 18:14:53 +0530

    improved file inputs

--- demo1.c
+++ demo1.c
@@ -5,7 +5,7 @@ int main()
 	
 	
 	 int arrival_time[10]={1,1,1,1,1,1,1,1,1,1}, burst_time[10], temp[10];
-      int i, smallest, count = 0, time, limit;
+      int i, smallest, count = 0, time, limit,count_infile=0;
       int wait_time[10],turnaround_time[10],completion[10], end;
       float average_waiting_time=0, average_turnaround_time=0;
 	
@@ -13,38 +13,41 @@ int main()
 	 FILE* file = fopen ("test.txt", "r");
   int h = 0,j,k;
   int array[100];
-  fscanf (file, "%d", &h);    
-  for (j=0; (!feof (file)); j++)
+  fscanf (file, "%d", &h); 
+  array[0] = h;   
+  for (j=1; (!feof (file)); j++)
     {
      // printf ("%d\n", i);
-      array[j] = h;
-      fscanf (file, "%d", &h);      
+      //array[j] = h;
+      fscanf (file, "%d", &h); 
+	  array[j] = h;     
     }
     for(k=0; k<j; k++)	{
-    	printf("%d\n", array[k]);
+    	printf("%d\n", array[k]);  	
 	}
   fclose (file);  
   for(j=0;j<100;j++)
-  {
+  {	
   burst_time[j]=array[j];
-  
-}
+	}
+	
 for(j=0;burst_time[j]!='\0';j++)
-printf("%d\n",burst_time[j]);
-  
+printf("\n%d",burst_time[j]);
+
+	printf("\n count of burst times %d",j);  
+	limit=j;
+	printf("\nNumber of processes %d\n",limit); 
 	
-	
-	
-     
-      printf("\nEnter the Total Number of Processes:\t");
-      scanf("%d", &limit); 
-      printf("\nEnter Details of %d Processes\n", limit);
+    
+//      printf("\nEnter the Total Number of Processes:\t");
+//      scanf("%d", &limit); 
+//      printf("\nEnter Details of %d Processes\n", limit);
       for(i = 0; i < limit; i++)
       {
             //printf("\nEnter Arrival Time:\t");
             //scanf("%d", &arrival_time[i]);
-            printf("Enter Burst Time:\t");
-            scanf("%d", &burst_time[i]); 
+            //printf("Enter Burst Time:\t");
+            //scanf("%d", &burst_time[i]); 
             temp[i] = burst_time[i];
       }
       burst_time[9] = 9999;  
@@ -82,8 +85,10 @@ printf("%d\n",burst_time[j]);
    		average_turnaround_time = average_turnaround_time + turnaround_time[i];
     	
 	}
+	average_waiting_time = average_waiting_time/limit;
+	average_turnaround_time = average_turnaround_time/limit;
 	  
-      printf("\n\nAverage Waiting Time:\t%lf\n", average_waiting_time/limit);
-      printf("Average Turnaround Time:\t%lf\n", average_turnaround_time/limit);  
+      printf("\n\nAverage Waiting Time:\t%lf\n", average_waiting_time);
+      printf("Average Turnaround Time:\t%lf\n", average_turnaround_time);  
       return 0;
 }
