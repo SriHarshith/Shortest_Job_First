4bbc5fa3c63ecdcf800a1ecd3905df1c4c4b7460
SriHarshith <34154723+SriHarshith@users.noreply.github.com>
2019-03-21 18:17:34 +0530

    added comments and made code clear

--- demo1.c
+++ demo1.c
@@ -1,41 +1,39 @@
 #include <stdio.h>
-
 int main() 
-{
+{ 
 	
-	
-	 int arrival_time[10]={1,1,1,1,1,1,1,1,1,1}, burst_time[10], temp[10];
-      int i, smallest, count = 0, time, limit,count_infile=0;
-      int wait_time[10],turnaround_time[10],completion[10], end;
-      float average_waiting_time=0, average_turnaround_time=0;
-	
-	
+	int arrival_time[10]={0}, burst_time[10], temp[10];
+ 	int i, smallest, count = 0, time, limit,count_infile=0;
+    int wait_time[10],turnaround_time[10],completion[10], end;
+    float average_waiting_time=0, average_turnaround_time=0;
+    	
+    int h = 0,j,k;
+ 	int array[100];
+ 	 	
 	 FILE* file = fopen ("test.txt", "r");
-  int h = 0,j,k;
-  int array[100];
-  fscanf (file, "%d", &h); 
-  array[0] = h;   
-  for (j=1; (!feof (file)); j++)
-    {
-     // printf ("%d\n", i);
-      //array[j] = h;
-      fscanf (file, "%d", &h); 
-	  array[j] = h;     
-    }
-    for(k=0; k<j; k++)	{
-    	printf("%d\n", array[k]);  	
-	}
-  fclose (file);  
-  for(j=0;j<100;j++)
-  {	
-  burst_time[j]=array[j];
-	}
+  	 fscanf (file, "%d", &h);   //taking 1st number of file
+  	 array[0] = h;				//storing at 1st position of array
+	      
+    for (j=1; (!feof (file)); j++)
+     {
+      fscanf (file, "%d", &h);   //taking 2nd element onwards till last element
+	  array[j] = h;  			 //storing them in array  
+     }
+     
+    for(k=0; k<j; k++)
+    printf("%d\n", array[k]);  	 //printing elements of array
 	
-for(j=0;burst_time[j]!='\0';j++)
-printf("\n%d",burst_time[j]);
+    fclose (file);               //closing the file
+    
+    for(j=0;j<100;j++)
+    burst_time[j]=array[j];      //copy array elements to burst_time array
+	
+	for(j=0;burst_time[j]!='\0';j++)
+	printf("\n%d",burst_time[j]); //printing burst_time array elements
 
-	printf("\n count of burst times %d",j);  
-	limit=j;
+
+	printf("\n count of burst time array %d",j);  
+    limit=j;
 	printf("\nNumber of processes %d\n",limit); 
 	
     
@@ -45,12 +43,12 @@ printf("\n%d",burst_time[j]);
       for(i = 0; i < limit; i++)
       {
             //printf("\nEnter Arrival Time:\t");
-            //scanf("%d", &arrival_time[i]);
-            //printf("Enter Burst Time:\t");
             //scanf("%d", &burst_time[i]); 
             temp[i] = burst_time[i];
       }
-      burst_time[9] = 9999;  
+      
+      burst_time[9] = 9999;  		 //assigning a large value to last element of array 
+      
       for(time = 0; count != limit; time++)
       {
             smallest = 9;
